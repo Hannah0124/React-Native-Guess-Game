66d0accd4f695b8d51bd035e250a8b4fd75c9ecf
Hannah <hannahjoo24@gmail.com>
2020-07-01 15:12:12 -0700

    Add local/network images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText';
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <BodyText>The Game is Over!</BodyText>
+      <View style={styles.imageContainer}>
+        <Image 
+          fadeDuration={300}
+          // source={require('../assets/success.png')} 
+          source={{uri: 'https://cdn.dribbble.com/users/4851961/screenshots/10129725/dribble2_4x.jpg?compress=1&resize=1200x900'}}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      {/* resizeMode: cover (default) => keeps image ratio */}
+
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -17,6 +29,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150, // half of wieth and height
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   }
 });
 
