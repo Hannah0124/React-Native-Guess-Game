ad809548e1857efc88292c49445c45b49481111b
Hannah <hannahjoo24@gmail.com>
2020-07-01 22:29:30 -0700

    Rendering different layouts

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -51,10 +51,27 @@ const GameScreen = props => {
 
   const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
 
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(Dimensions.get('window').width);
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(Dimensions.get('window').height);
+
   const currentLow = useRef(1); // useRef => the component will not re-render!
   const currentHigh = useRef(100);
 
   const { userChoice, onGameOver } = props; // destructuring
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get('window').width);
+      setAvailableDeviceHeight(Dimensions.get('window').height);
+    };
+
+    Dimensions.addEventListener('change', updateLayout);
+
+    return () => {
+      Dimensions.removeEventListener('change', updateLayout);
+    }
+  });
+
 
   useEffect(() => {
     if (currentGuess === userChoice) {
@@ -86,13 +103,40 @@ const GameScreen = props => {
 
     // setRounds(currentRounds => currentRounds + 1); // increment by 1
     setPastGuesses(currentPastGuesses => [nextNumber.toString(), ...currentPastGuesses]);
-
   };
 
   let listContainerStyle = styles.listContainer;
 
-  if (Dimensions.get('window').width < 350) {
+  if (availableDeviceWidth < 350) {
     listContainerStyle = styles.listContainerBig;
+  }
+
+  if (availableDeviceHeight < 500) {
+    return (
+      <View style={styles.screen}>
+        <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+        <View style={styles.controls}>
+          <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+            <Ionicons name="md-remove" size={24} color="white" />
+          </MainButton>
+
+          <NumberContainer>{currentGuess}</NumberContainer>
+          
+          <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            <Ionicons name="md-add" size={24} color="white" />
+          </MainButton>
+        </View>
+
+        <View style={styles.listContainer}>
+          <FlatList 
+            keyExtractor={(item) => item} 
+            data={pastGuesses} 
+            renderItem={renderListItem.bind(this, pastGuesses.length)} 
+            contentContainerStyle={styles.list}
+          />
+        </View>
+      </View>
+    );
   }
 
   return (
@@ -138,6 +182,12 @@ const styles = StyleSheet.create({
     marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%'
+  },
+  controls: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '80%'
   },
   listContainer: {
     flex: 1, // for scrollView (Android)
